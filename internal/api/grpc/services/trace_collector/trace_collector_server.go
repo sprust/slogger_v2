d2c965a5829297da29f05f94af0fec77d632e7b2
spp28 <sppetrovich@mail.ru>
2025-01-12 15:57:59 +0200

    collector: token detecting

--- internal/api/grpc/services/trace_collector/trace_collector_server.go
+++ internal/api/grpc/services/trace_collector/trace_collector_server.go
@@ -2,9 +2,15 @@ package trace_collector
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"google.golang.org/grpc/metadata"
 	"log/slog"
 	gen "slogger/internal/api/grpc/gen/services/trace_collector_gen"
+	"strings"
 )
+
+const bearerPrefix = "Bearer "
 
 type Server struct {
 	gen.UnimplementedTraceCollectorServer
@@ -16,14 +22,36 @@ func NewServer() (*Server, error) {
 
 func (c *Server) Create(ctx context.Context, in *gen.TraceCreateRequest) (*gen.TraceCollectorResponse, error) {
 	// TODO: Implement this method
-	slog.Info("Creating trace...")
+
+	go func() {
+		token, err := c.detectBearerToken(ctx)
+
+		if err != nil {
+			slog.Error("Error detecting bearer token: " + err.Error())
+
+			return
+		}
+
+		slog.Info("Creating trace: " + token)
+	}()
 
 	return &gen.TraceCollectorResponse{StatusCode: 200, Message: "ok"}, nil
 }
 
 func (c *Server) Update(ctx context.Context, in *gen.TraceUpdateRequest) (*gen.TraceCollectorResponse, error) {
 	// TODO: Implement this method
-	slog.Info("Updating trace...")
+
+	go func() {
+		token, err := c.detectBearerToken(ctx)
+
+		if err != nil {
+			slog.Error("Error detecting bearer token: " + err.Error())
+
+			return
+		}
+
+		slog.Info("Updating trace: " + token)
+	}()
 
 	return &gen.TraceCollectorResponse{StatusCode: 200, Message: "ok"}, nil
 }
@@ -32,4 +60,36 @@ func (c *Server) Close() error {
 	slog.Warn("grpc: closing trace collector server...")
 
 	return nil
+}
+
+func (c *Server) detectBearerToken(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+
+	if !ok {
+		return "", fmt.Errorf("missing metadata")
+	}
+
+	authValues := md.Get("authorization")
+
+	if len(authValues) == 0 {
+		return "", errors.New("missing authorization")
+	}
+
+	authorization := authValues[0]
+
+	if authorization == "" {
+		return "", errors.New("missing authorization value")
+	}
+
+	if !strings.HasPrefix(authorization, bearerPrefix) {
+		return "", errors.New("invalid authorization value")
+	}
+
+	token := strings.Trim(strings.TrimPrefix(authorization, bearerPrefix), " ")
+
+	if token == "" {
+		return "", errors.New("invalid authorization value")
+	}
+
+	return token, nil
 }
--- makefile
+++ makefile
@@ -8,6 +8,9 @@ build:
 	CGO_ENABLED=0 GOOS=linux go build -v -a -o ./bin/slogger ./cmd/slogger/main.go \
 		&& chmod +x ./bin/slogger
 
+slogger:
+	./bin/slogger ${c}
+
 grpc-generate:
 	protoc --go_out=./internal/api/grpc/gen/ \
 		--go-grpc_out=./internal/api/grpc/gen/ \
